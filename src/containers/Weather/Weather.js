c21d8267808491af1cb6040f8e9560deddbe9b48
James Hoskin <J_hoskin@outlook.com>
2019-12-09 20:32:33 +1000

    Add first contents and routing to Weather component

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -1,12 +1,17 @@
-import React from './node_modules/react';
-import './weather.css';
+import React, {Component} from 'react';
+import './Weather.css';
+import Header from '../../components/Header/Header';
+import Divider from '../../components/Divider/Divider';
 
-const weather = () => {
-    return (
-        <div className = "Weather">
-
-        </div>
-    )
+class Weather extends Component {
+    render() {
+        return (
+            <div className = "Weather">
+                <Header pageName = "Weather" />
+                <Divider />
+            </div>
+        )
+    }
 }
 
-export default weather;
+export default Weather;
