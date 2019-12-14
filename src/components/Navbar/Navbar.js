34e9e1e513567d96e41d9ff401c025ad55e62bcf
James Hoskin <J_hoskin@outlook.com>
2019-12-14 23:49:35 +1000

    Add new Home icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
-import HomeIcon from '../../images/home-icon.png'
+import HomeIcon from '../../images/homeIcon.svg'
 
 const navbar = () => {
     return (
         <Link to="" >
             <div className = "NavBar">
-                <img src={HomeIcon} alt="home icon" width="28px" height="28px"/>
+                <img src={HomeIcon} alt="home icon" height="28px"/>
             </div>
         </Link>
     )
